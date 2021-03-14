55bf4c4bcc70fdc0d919eff8556b222becb9dc2b
Maxim Zebzeev <maksimzeb@yandex.ru>
2021-03-14 19:24:06 +0500

    Recopy

---
+++ App.js
@@ -0,0 +1,10 @@
+import './App.css';
+import SimpleForm from './SimpleForm';
+
+const App = () => (
+  <div className='App'>
+    <SimpleForm />
+  </div>
+)
+
+export default App;
---
+++ Greetings.js
@@ -0,0 +1,8 @@
+import React from 'react' ;
+
+const Greetings = (props) => 
+    <div> 
+        Hey you! {props.firstName} {props.lastName} !
+    </div>
+
+export default Greetings;
